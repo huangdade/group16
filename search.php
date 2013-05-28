bfd21b63879cbd8043f726087bfb8635cfbc153f
huangdade <bfbrmt@163.com>
2013-05-28 17:59:28 +0800

    update

--- search.php
+++ search.php
@@ -1,7 +1,18 @@
 <html>
 <head>
+	<title>search result</title>
 </head>
 <body>
+<?php
+try {
+	$conn = new PDO("sqlsrv:server = tcp:jzcdokmf78.database.windows.net,1433; Database = goods_search_system", "common", "A0\/!a6609dsq");
+	$conn->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
+}
+catch ( PDOException $e ) {
+	print( "Error connecting to SQL Server." );
+	die(print_r($e));
+}
 
+?>
 </body>
 </html>
