ec798561e5a63d7b9ef845d32a1aa24f248891fe
huangdade <bfbrmt@163.com>
2013-05-29 23:45:07 +0800

    Update search.php

--- search.php
+++ search.php
@@ -4,7 +4,7 @@ PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 	<title>search result</title>
-	<meta http-equiv="charset" content="utf-8"/>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 	<link rel="stylesheet" type="text/css" href="css/search.css" />
 </head>
 <body>
