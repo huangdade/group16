cc36e3ad9fd501beede7ce3f634ae4fa5bc518ef
huangdade <bfbrmt@163.com>
2013-05-29 20:08:01 +0800

    Update search.php

--- search.php
+++ search.php
@@ -1,6 +1,8 @@
-<html>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1 Transitional//EN" "http://www.w3.org/TR/xhtml11/DTD/xhtml11-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 	<title>search result</title>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
 	<link rel="stylesheet" type="text/css" href="css/search.css" />
 </head>
 <body>
